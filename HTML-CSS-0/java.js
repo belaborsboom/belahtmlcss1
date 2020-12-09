7f1c6d2ca73c02db600eb2d411236da9e0244177
Bela Borsboom <56720528+belaborsboom@users.noreply.github.com>
2020-12-09 11:42:27 +0100

    Update java.js

--- HTML-CSS-0/java.js
+++ HTML-CSS-0/java.js
@@ -2,7 +2,8 @@ let plaatje = document.getElementById("tijger");
 plaatje.addEventListener("click", plaatgroot)
 function plaatgroot(){
   plaatje.outerHTML = '<img id="tijger2" src="https://belaborsboom.github.io/HTML-CSS-0/belatomaat.jpeg"></img>';
+  let plaatje2 = document.getElementById("tijger2");
+  plaatje2.style.heigth = 117px;
+  plaatje2.style.width = 100px;
 }
-let plaatje2 = document.getElementById("tijger2");
-plaatje2.style.heigth = 117px;
-plaatje2.style.width = 100px;
+
